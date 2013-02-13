cf400d7725482c92532665ba3e4c073ae65dc664
Guillaume Besson <geek.uillaume@gmail.com>
2013-02-13 11:37:27 +0100

    Initial commit

---
+++ activation.py
@@ -0,0 +1,52 @@
+import sublime, sublime_plugin
+
+class EpiActivate(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    # self.view.set_syntax_file("Packages/SublimTek/SublimTek.tmLanguage")
+    tmp = self.view.settings().get("auto_indent")
+    self.view.settings().set("auto_indent_bak", tmp)
+    self.view.settings().set("auto_indent", "false")
+
+    tmp = self.view.settings().get("detect_indentation")
+    self.view.settings().set("detect_indentation_bak", tmp)
+    self.view.settings().set("detect_indentation", "false")
+
+    tmp = self.view.settings().get("rulers")
+    self.view.settings().set("rulers_bak", tmp)
+    self.view.settings().set("rulers", [80])
+
+    tmp = self.view.settings().get("tab_size")
+    self.view.settings().set("tab_size_bak", tmp)
+    self.view.settings().set("tab_size", 8)
+
+    tmp = self.view.settings().get("translate_tabs_to_spaces")
+    self.view.settings().set("translate_tabs_to_spaces_bak", tmp)
+    self.view.settings().set("translate_tabs_to_spaces", "false")
+
+    tmp = self.view.settings().get("trim_trailing_white_space_on_save")
+    self.view.settings().set("trim_trailing_white_space_on_save_bak", tmp)
+    self.view.settings().set("trim_trailing_white_space_on_save", "true")
+
+    self.view.settings().set("sublim_tek_mode", "true")
+
+class EpiDeactivate(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    tmp = self.view.settings().get("auto_indent_bak")
+    self.view.settings().set("auto_indent", tmp)
+
+    tmp = self.view.settings().get("detect_indentation_bak")
+    self.view.settings().set("detect_indentation", tmp)
+
+    tmp = self.view.settings().get("rulers_bak")
+    self.view.settings().set("rulers", tmp)
+
+    tmp = self.view.settings().get("tab_size_bak")
+    self.view.settings().set("tab_size", tmp)
+
+    tmp = self.view.settings().get("translate_tabs_to_spaces_bak")
+    self.view.settings().set("translate_tabs_to_spaces", tmp)
+
+    tmp = self.view.settings().get("trim_trailing_white_space_on_save_bak")
+    self.view.settings().set("trim_trailing_white_space_on_save", tmp)
---
+++ align.py
@@ -0,0 +1,60 @@
+import sublime, sublime_plugin
+import re
+
+def is_function(line):
+  if re.match(r"^[a-zA-Z0-9_]+\t[*]*[a-zA-Z0-9_]+\([a-zA-Z0-9_*, ]*\)$", line):
+    return 1
+  return 0
+
+def is_condition(line):
+  if re.match(r"^.*(if|else if|else|while|for|do)([ ]\(.*\)){0,1}$", line):
+    return 1
+  return 0
+
+def ifloor(number, floor):
+  if number < floor:
+    return floor
+  return number
+
+class EpiAlignAllCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view.run_command("epi_indent_tabs")
+
+class EpiIndentAll(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    lines = self.view.substr(sublime.Region(0, self.view.size()))
+    i = -1
+    last_spaces = 0
+    last_line = ""
+    for line in lines.split("\n"):
+      i += 1
+      actual_spaces = 0
+      new_spaces = 0
+      next_line = 0
+      for ch in line:
+        if ch == ' ' or ch == '\t': actual_spaces += 1
+        elif ch == '{':
+          if not is_function(last_line):
+            new_spaces += 2
+          next_line += 2
+          break
+        elif ch == '}':
+          new_spaces -= 2
+          next_line -= 2
+          break
+        else:
+          if is_condition(last_line):
+            new_spaces += 2
+            next_line -= 2
+          break
+      str_spaces = "\t" * ((new_spaces + last_spaces) / 8) + " " * ((new_spaces + last_spaces) % 8)
+      self.view.replace(edit, sublime.Region(self.view.text_point(i, 0), self.view.text_point(i, actual_spaces)), str_spaces)
+      last_spaces = ifloor(new_spaces + next_line + last_spaces, 0)
+      last_line = line
+
+
+class EpiHooks(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+      if view.settings().get('sublim_tek_mode') == "true":
+        view.run_command('epi_indent_all')
